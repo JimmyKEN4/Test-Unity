f59581167d42aac930542d212c5bc43ac23f5448
DESKTOP-GT9RQJO\Samji <jimmyken4@hotmail.com>
2020-08-23 19:06:10 +0800

    Testing First Push

---
+++ Assets/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [Header("Testing Variable")]
+    [SerializeField] float _numofLetter;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        _numofLetter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddTest();
+    }
+
+    void AddTest()
+    {
+        if (_numofLetter >= 150)
+            return;
+
+        for (int i = 0; i < 15; i++)
+        {
+            _numofLetter++;
+            Debug.Log("Numn: " + _numofLetter);
+        }
+    }
+}
